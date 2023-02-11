0edff2640028944c2b9ca5ccbda9da34633c8ac5
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-11 00:32:06 -0600

    news component

--- src/lib/components/News.svelte
+++ src/lib/components/News.svelte
@@ -1,13 +1,29 @@
 <section id="news" class="bg-color-cream text-color-background m-auto w-full pt-20">
 	<div class="m-auto flex w-[95%] justify-center gap-6 pb-10">
-		<div class="bg-color-green text-color-cream h-[40vmin] w-3/5 rounded-2xl">Big news</div>
+		<div class="bg-color-green text-color-cream h-[60vmin] w-3/5 rounded-3xl">
+			<div class="ml-10 mr-10 mt-5 flex items-center justify-around">
+				<h2 class="font-playfair text-5xl font-bold">Big News</h2>
+				<img src="img/news-coffee.svg" alt="coffee" />
+			</div>
+			<p class="mt-10 ml-10 mr-10 mb-0">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita at dolor adipisci libero
+				nihil! Eos eaque, repellendus ipsam commodi laborum unde, omnis impedit, amet perspiciatis a
+				voluptates doloremque maiores quae.
+			</p>
+			<div class="ml-10 mr-10 mt-10 flex justify-end">
+				<button
+					class="text-color-cream font-merriweather rounded-3xl border-[1px] pl-6 pr-6 pt-2 pb-2 text-right"
+					>Read More</button
+				>
+			</div>
+		</div>
 		<div
-			class="bg-color-background text-color-cream flex h-[40vmin] w-32 items-center justify-center rounded-2xl"
+			class="bg-color-background text-color-cream flex h-[60vmin] w-32 items-center justify-center rounded-3xl"
 		>
 			<p class="v-text rotate-180 transform text-5xl font-bold uppercase">Coffee Shop</p>
 		</div>
 		<div
-			class="bg-color-background text-color-cream flex h-[40vmin] w-32 items-center justify-center rounded-2xl"
+			class="bg-color-background text-color-cream flex h-[60vmin] w-32 items-center justify-center rounded-3xl"
 		>
 			<p class="v-text rotate-180 transform text-5xl font-bold uppercase">Restaurant</p>
 		</div>
