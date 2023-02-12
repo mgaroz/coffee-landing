f67f9d8bd98b5a594c6b3499baac076dd4f62924
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-11 22:17:07 -0600

    social links effects

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,8 +1,8 @@
 <div class="bg-color-green w-full pb-10">
 	<div class="text-color-white font-merriweather m-auto w-[95%] pt-10">
 		<div class="grid grid-cols-4 gap-8">
-			<div>
-				<img src="img/logo.png" alt="logo_footer" class="h-auto w-[15vmin]" />
+			<div class="w-full self-start">
+				<img src="img/logo.png" alt="logo_footer" class="m-auto h-auto w-[15vmin]" />
 			</div>
 			<div class="border-r-2">
 				<p class="p-6 leading-loose">
@@ -11,7 +11,7 @@
 					quaerat magnam nobis! Assumenda, nihil. Sed.
 				</p>
 			</div>
-			<div class="ml-10 mr-10 text-center">
+			<div class="ml-10 mr-10 self-center text-center">
 				<ul class="font-playfair columns-2 leading-loose">
 					<li><a href="/">Home</a></li>
 					<li><a href="#about">About</a></li>
@@ -20,13 +20,20 @@
 					<li><a href="#contact">Contact</a></li>
 				</ul>
 			</div>
-			<div class="font-playfair w-[30vmin] items-center justify-center">
-				<h3>Social Links</h3>
+			<div class="font-playfair w-[30vmin] self-center">
 				<div class="text-2xl font-light">
-					<a href="/" class="mr-2"><i class="fa-brands fa-twitter" /></a>
-					<a href="/" class="mr-2"><i class="fa-brands fa-youtube" /></a>
-					<a href="/" class="mr-2"><i class="fa-brands fa-linkedin-in" /></a>
-					<a href="/" class="mr-2"><i class="fa-brands fa-instagram" /></a>
+					<a href="/" class="mr-2"
+						><i class="fa-brands fa-twitter duration-150 hover:scale-125" /></a
+					>
+					<a href="/" class="mr-2"
+						><i class="fa-brands fa-youtube duration-150 hover:scale-125" /></a
+					>
+					<a href="/" class="mr-2"
+						><i class="fa-brands fa-linkedin-in duration-150 hover:scale-125" /></a
+					>
+					<a href="/" class="mr-2"
+						><i class="fa-brands fa-instagram duration-150 hover:scale-125" /></a
+					>
 				</div>
 			</div>
 		</div>
