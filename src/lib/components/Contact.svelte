3ce8f67f27acd2a05b3669fb9a7c8061dcd55793
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-11 19:46:06 -0600

    finished contact component

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -1,7 +1,32 @@
 <section id="contact" class="bg-color-cream w-full pt-20">
-	<div>
+	<div class="m-auto w-[95%]">
 		<h2 class="font-merriweather text-center text-7xl uppercase">
 			Contact <span class="text-color-gray-text">Us</span>
 		</h2>
+	</div>
+	<div class="m-auto mt-10 flex w-[95%] gap-16 pb-20">
+		<img src="img/contact.svg" alt="contact us" class="w-1/2" />
+		<div class="font-merriweather flex w-1/3 flex-col items-center justify-center text-3xl">
+			<div class="flex w-full flex-col gap-4 text-3xl">
+				<div class="flex justify-between">
+					<p><span class="text-color-gray-text">Tel</span></p>
+					<div class="w-[250px] text-left">
+						<p>+555 555 555</p>
+					</div>
+				</div>
+				<div class="flex justify-between">
+					<p><span class="text-color-gray-text">Address</span></p>
+					<div class="w-[250px] text-left">
+						<p>C. Ribera, 4, 33150 Cudillero, Asturias, Spain</p>
+					</div>
+				</div>
+				<div class="flex justify-between">
+					<p><span class="text-color-gray-text">Email</span></p>
+					<div class="w-[250px] text-left">
+						<p>info@andys.es</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
