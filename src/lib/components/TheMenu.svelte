2d7fe0f356cbaa6bac83dbe3324641866948d9cd
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-13 05:17:45 -0600

    replaced image format

--- src/lib/components/TheMenu.svelte
+++ src/lib/components/TheMenu.svelte
@@ -5,34 +5,34 @@
 		Espresso: {
 			price: 6,
 			description: 'A concentrated form of coffee served in small, strong shots',
-			img: 'img/coffee/espresso.jpg'
+			img: 'img/coffee/espresso.webp'
 		},
 		Capuccino: {
 			price: 5,
 			description: 'A coffee drink made from a mixture of espresso and steamed milk',
-			img: 'img/coffee/capuccino.jpg'
+			img: 'img/coffee/capuccino.webp'
 		},
 		Americano: {
 			price: 3,
 			description: 'An Americano is espresso topped with hot water',
-			img: 'img/coffee/americano.jpg'
+			img: 'img/coffee/americano.webp'
 		},
 		'Latte Art': {
 			price: 8,
 			description:
 				'Latte art is the design you see on top of (typically) drinks like lattes, cappuccinos, and similar milk-and-espresso beverages',
-			img: 'img/coffee/latte-art.jpg'
+			img: 'img/coffee/latte-art.webp'
 		},
 		Doppio: {
 			price: 5,
 			description:
 				'The name doppio means ”double” in Italian and refers to a double shot or two espresso shots',
-			img: 'img/coffee/doppio.jpg'
+			img: 'img/coffee/doppio.webp'
 		},
 		Cortado: {
 			price: 7,
 			description: 'A cortado is a hot espresso beverage, coupled with warm, steamed milk',
-			img: 'img/coffee/cortado.jpg'
+			img: 'img/coffee/cortado.webp'
 		}
 	};
 </script>
--- svelte.config.js
+++ svelte.config.js
@@ -7,6 +7,7 @@ const config = {
 		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
+
 };
 
 export default config;
