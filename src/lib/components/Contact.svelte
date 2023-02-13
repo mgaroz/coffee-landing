ba7eec4641f0901cbf23e8d839d209781ef43e11
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-13 00:33:48 -0600

    added contact form

--- src/lib/components/Contact.svelte
+++ src/lib/components/Contact.svelte
@@ -5,28 +5,42 @@
 		</h2>
 	</div>
 	<div class="m-auto mt-10 flex w-[95%] gap-16 pb-20">
-		<img src="img/contact.svg" alt="contact us" class="w-1/2" />
-		<div class="font-merriweather flex w-1/3 flex-col items-center justify-center text-3xl">
-			<div class="flex w-full flex-col gap-4 text-3xl">
+		<div class="flex w-1/2 justify-center">
+			<img src="img/contact.svg" alt="contact us" />
+		</div>
+		<div class="font-merriweather flex w-1/3 flex-col items-center justify-evenly">
+			<div class="flex w-full flex-col gap-8 text-2xl">
 				<div class="flex justify-between">
-					<p><span class="text-color-gray-text">Phone</span></p>
+					<p><span class="text-color-gray-text text-xl">Phone</span></p>
 					<div class="w-[250px] text-left">
 						<p>+555 555 555</p>
 					</div>
 				</div>
 				<div class="flex justify-between">
-					<p><span class="text-color-gray-text">Address</span></p>
+					<p><span class="text-color-gray-text text-xl">Address</span></p>
 					<div class="w-[250px] text-left">
 						<p>C. Ribera, 4, 33150 Cudillero, Asturias, Spain</p>
 					</div>
 				</div>
 				<div class="flex justify-between">
-					<p><span class="text-color-gray-text">Email</span></p>
+					<p><span class="text-color-gray-text text-xl">Email</span></p>
 					<div class="w-[250px] text-left">
 						<p>info@andys.es</p>
 					</div>
 				</div>
+				<div class="flex justify-start gap-4 text-base">
+					<input
+						type="email"
+						class="bg-color-cream border-b-[1px] focus:border-b-2 focus:outline-none active:border-0"
+						placeholder="Enter your email"
+					/>
+					<button
+						class="text-color-background hover:bg-color-green hover:text-color-white rounded-3xl border-[1px] pt-2 pb-2 pl-4 pr-4 font-sans duration-150"
+						>Get in touch</button
+					>
+				</div>
 			</div>
+			<div class="flex w-full justify-evenly gap-4" />
 		</div>
 	</div>
 </section>
