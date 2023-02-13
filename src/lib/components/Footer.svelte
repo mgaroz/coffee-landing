942b5098b1f769592ff2148b9a0cc1fec7b3cb0d
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-12 23:00:43 -0600

    added margin to contact component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -10,7 +10,7 @@
 	<div class="text-color-white font-merriweather m-auto w-[95%] pt-10">
 		<div class="grid grid-cols-4 gap-8">
 			<div class="w-full self-start">
-				<a href="/" on:click={() => activeItem.set('home')}>
+				<a href="/" on:click={() => setActiveItem('home')}>
 					<img
 						src="img/logo.png"
 						alt="logo_footer"
