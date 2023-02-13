8d174aaf75164ea0c226b018c0b6e2f6ec01dda8
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-13 03:28:17 -0600

    changed wording

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -7,10 +7,10 @@
 		class="border-b-color-gray-text m-auto mt-4 w-[95%] border-t-0 border-l-0 border-r-0 border-b-2"
 	/>
 	<div class="m-auto mt-20 grid w-[95%] grid-cols-3">
-		<h2 class="text-color-gray-text font-merriweather text-[22px] uppercase">About Annie's</h2>
-		<p class="text-color-cream font-merriweather col-span-2 text-[2rem] leading-relaxed">
+		<h2 class="text-color-gray-text font-merriweather text-[22px] uppercase">About Andy's</h2>
+		<p class="text-color-white font-merriweather col-span-2 text-[2rem] leading-relaxed">
 			Andy's is a coffee and restaurant shop located in Asturias with a semi modern concept. Opened
-			in 2023, it is quite a spacious and comfortable place for families and everyone due to the
+			in 2023, it is quite a spacious and comfortable place for families and anyone due to the
 			surrounding beautiful, natural scenery.
 		</p>
 	</div>
