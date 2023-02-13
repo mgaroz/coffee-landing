79f44cd88f5b810aed1f2e5e05de2bab1aafecb8
Miguel Garoz <mgaroz@umes.edu.gt>
2023-02-13 00:02:07 -0600

    testimonials, better formatting

--- src/lib/components/Testimonials.svelte
+++ src/lib/components/Testimonials.svelte
@@ -12,16 +12,16 @@
 			<div class="flex w-[40vmin] flex-col gap-6">
 				<div class="border-color-gray-text border-2 pt-8 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Best coffee shop in Spain</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, fugit? Reprehenderit quo
 						eligendi, officia illum earum odio quae fugit, quam voluptates soluta eaque! Quas culpa
 						cum laudantium accusamus animi. Deserunt.
 					</p>
-					<p class="text-right">Jean Pierre</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Jean Pierre</p>
 				</div>
 				<div class="border-color-gray-text border-2 pt-8 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Excellent service</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolor similique minus
 						labore laudantium. Ipsam quis tempore nihil id ipsa, iusto enim sint saepe porro,
 						recusandae libero. Quae, iste accusamus! Aut, praesentium obcaecati commodi velit nihil
@@ -29,12 +29,13 @@
 						cumque at, ducimus qui voluptate doloremque nesciunt! Reiciendis ab repudiandae dolores
 						doloremque tempora!
 					</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Jules Moerman</p>
 				</div>
 			</div>
 			<div class="flex w-[40vmin] flex-col gap-6">
 				<div class="border-color-gray-text border-2 pt-5 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Impeccable</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis dolor similique minus
 						labore laudantium. Ipsam quis tempore nihil id ipsa, iusto enim sint saepe porro,
 						recusandae libero. Quae, iste accusamus! Aut, praesentium obcaecati commodi velit nihil
@@ -42,32 +43,36 @@
 						cumque at, ducimus qui voluptate doloremque nesciunt! Reiciendis ab repudiandae dolores
 						doloremque tempora!
 					</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Otmar Ježek</p>
 				</div>
 				<div class="border-color-gray-text border-2 pt-5 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Recommend 100%</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis ad molestiae,
 						repellendus nesciunt voluptatibus culpa consectetur maxime non impedit iste voluptates
 						assumenda sequi earum animi aspernatur! Sequi molestias quis quisquam.
 					</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Erik Vaquero</p>
 				</div>
 			</div>
 			<div class="flex w-[40vmin] flex-col gap-6">
 				<div class="border-color-gray-text border-2 pt-5 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Would come back</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti! Voluptates
 						delectus libero accusantium optio cupiditate. Aspernatur blanditiis quaerat voluptatum
 						officiis asperiores alias! Eveniet enim ipsam recusandae error dicta ullam.
 					</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Nicolas Benett</p>
 				</div>
 				<div class="border-color-gray-text border-2 pt-5 pl-5 pr-5 pb-5">
 					<h3 class="text-2xl">Masters of their craft</h3>
-					<p class="font-merriweather mt-8 text-sm leading-loose">
+					<p class="font-merriweather mt-8 text-sm leading-loose text-[#a2a2a2]">
 						Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti! Voluptates
 						delectus libero accusantium optio cupiditate. Aspernatur blanditiis quaerat voluptatum
 						officiis asperiores alias! Eveniet enim ipsam recusandae error dicta ullam.
 					</p>
+					<p class="font-merriweather mt-4 text-right text-[#b7b7b7]">Leo Montes</p>
 				</div>
 			</div>
 		</div>
