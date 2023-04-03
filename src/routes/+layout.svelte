8cee866226007257bd8d241290e4f48db4e78daa
Miguel Garoz <mgaroz@gmail.com>
2023-04-03 04:14:47 -0600

    testing preload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,11 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Playfair+Display:wght@400;600;700&family=Poppins:wght@300;400;700&display=swap"
+		rel="preload"
+		as="style"
+	/>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Playfair+Display:wght@400;600;700&family=Poppins:wght@300;400;700&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
